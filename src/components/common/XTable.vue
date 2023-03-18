9acf204f2faf58db4598ad06769048ce6be5d8f0
Antony <antonyz.dev@gmail.com>
2023-03-18 20:11:51 -0300

    update XTable

--- src/components/common/XTable.vue
+++ src/components/common/XTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overflow-x-auto relative">
+  <div class="x-table overflow-x-auto relative">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
       <thead
         class="text-xs text-gray-700 uppercase bg-slate-200 dark:bg-gray-700 dark:text-gray-400"
